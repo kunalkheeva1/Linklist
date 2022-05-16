c13bb710f53738983e01d075812fdd3b2e3d5e18
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-05-15 22:44:25 -0700

    node class

---
+++ src/Linklist.java
@@ -0,0 +1,15 @@
+public class Linklist {
+    //creating a seperate class of node to acces the nodes
+    public class Node{
+        String data;
+        Node next;
+        Node(String data){
+            this.data= data;
+            this.next =null;
+        }
+    }
+    public static void main(String[] args) {
+        Linklist list = new Linklist();
+
+    }
+}
