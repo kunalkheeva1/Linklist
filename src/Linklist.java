85d00356907900bc7cb30e0838595e7a6cdf8453
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-05-15 22:56:56 -0700

    created addFirst method of the node

--- src/Linklist.java
+++ src/Linklist.java
@@ -1,13 +1,27 @@
 public class Linklist {
+    Node head;
     //creating a seperate class of node to acces the nodes
     public class Node{
-        String data;
-        Node next;
-        Node(String data){
+        String data;                 //data of the node
+        Node next;                 //pointer towards the next node
+        Node(String data){        //added a constructor to help in as object
             this.data= data;
             this.next =null;
         }
     }
+    //creating a function to add the elements from the first
+public void addFirst(String data){
+        Node newNode= new Node(data);
+        if(head==null){           //if head is null assign newNode to it
+            head =newNode;
+        }else{
+            newNode.next =head;  //new node will point to head if there is already a list
+            head =newNode;       //and the new head will be your added node
+        }
+
+}
+
+
     public static void main(String[] args) {
         Linklist list = new Linklist();
 
